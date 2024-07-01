b80b9ae87ab5b6b30f130d81d065dd7d9bec8f64
bosch <130572668+boschaiyadej@users.noreply.github.com>
2024-07-01 15:48:26 +0700

    feat: created api post product

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import productRouter from "./apps/products.js";
+import { client } from "./utils/db.js";
 
 const app = express();
 const port = 4001;
--- server/apps/products.js
+++ server/apps/products.js
@@ -1,12 +1,27 @@
 import { Router } from "express";
+import { db } from "../utils/db.js";
 
 const productRouter = Router();
 
-productRouter.get("/", (req, res) => {});
+productRouter.get("/", async (req, res) => {
+  const collection = db.collection("products");
+  const products = await collection.find({}).toArray();
+
+  return res.json({ data: products });
+});
 
 productRouter.get("/:id", (req, res) => {});
 
-productRouter.post("/", (req, res) => {});
+productRouter.post("/", async (req, res) => {
+  const collection = db.collection("products");
+
+  const productData = { ...req.body };
+  const product = await collection.insertOne(productData);
+
+  return res.json({
+    message: "Product has been created successfully",
+  });
+});
 
 productRouter.put("/:id", (req, res) => {});
 
